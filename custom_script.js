add5e970c6840767eaf0b73be4a5197c0468bf64
UmanshSarabhai <49119948+UmanshSarabhai@users.noreply.github.com>
2021-08-30 15:35:21 +0530

    Add files via upload

---
+++ custom_script.js
@@ -0,0 +1,11 @@
+function limited() {
+    alert("Only limited features available");
+}
+
+function cart() {
+    alert("Cart feature is not active at the moment")
+}
+
+function offer() {
+    alert("No new offers at the moment")
+}
